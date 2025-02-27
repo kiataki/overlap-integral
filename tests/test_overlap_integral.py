2a744833ec96490d2550b853ecae030042dda7a2
= <kiatakimatheus@gmail.com>
2025-02-26 22:22:57 -0300

    :sparkles: I added pytest!

--- tests/test_overlap_integral.py
+++ tests/test_overlap_integral.py
@@ -1,39 +1,29 @@
-
 import numpy as np
+import pytest
 from overlap_integral.overlap_integral import OverlapIntegral
 
-import plotly.io as pio
-pio.kaleido.scope.default_format = "png"
 
+@pytest.fixture
+def overlap_integral_instance():
+    return OverlapIntegral()
 
-def main():
-    np.random.seed(3)  # Set random seed for reproducibility
-
-    metrics = OverlapIntegral()
-
-    # Generate or load data
+def test_overlap_integral(overlap_integral_instance):
+    np.random.seed(3)  
+    
     data1 = np.random.normal(loc=30, scale=1, size=1000)
     data2 = np.random.normal(loc=30, scale=1.2, size=1000)
 
-    # Choose PDF method: 'kde' or 'gaussian'
-    pdf_method = 'gaussian'
+    function_type = "kde"
+    pdf_1 = overlap_integral_instance.get_pdf(data1, pdf_type=function_type)
+    pdf_2 = overlap_integral_instance.get_pdf(data2, pdf_type=function_type)
 
-    # Get PDFs
-    pdf_1 = metrics.get_pdf(data1, method=pdf_method)
-    pdf_2 = metrics.get_pdf(data2, method=pdf_method)
-
-    # Calculate overlap integral
     lower_limit = min(np.min(data1), np.min(data2)) - 12 * max(np.std(data1), np.std(data2))
     upper_limit = max(np.max(data1), np.max(data2)) + 12 * max(np.std(data1), np.std(data2))
-    integral, error = metrics.overlap_integral(pdf_1, pdf_2, lower_limit, upper_limit)
 
-    print(f"Overlap integral: {integral}")
-    print(f"Estimated error: {error}")
+    integral, error = overlap_integral_instance.overlap_integral(pdf_1, pdf_2, lower_limit, upper_limit)
 
-    # Plot distributions
-    fig = metrics.plot_distributions(pdf_1, pdf_2, integral, error, x_range=(lower_limit, upper_limit))
-    fig.write_image("overlap_plot.png")
-    ##fig.show()
+    assert 0 <= integral <= 1, "The integral value should be between 0 and 1"
+    assert error >= 0, "The error value should be non-negative"
+    assert isinstance(integral, float), "The integral must be a float"
+    assert isinstance(error, float), "The error must be a float"
 
-if __name__ == '__main__':
-    main()
